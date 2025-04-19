a50325718e987533b71f0d7d825e77738838fb88
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-04-19 09:37:45 -0300

    Alterações

--- envia.php
+++ envia.php
@@ -1,25 +1,31 @@
 <?php
 
-    $nome = addslashes($_POST['nome']);
-    $email = addslashes($_POST['email']);
-    $contato = addslashes($_POST['contato']);
-    $mensagem = addslashes($_POST['mensagem']);
+$nome = filter_var($_POST['nome'], FILTER_SANITIZE_STRING);
+$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+$contato = filter_var($_POST['contato'], FILTER_SANITIZE_STRING);
+$mensagem = filter_var($_POST['mensagem'], FILTER_SANITIZE_STRING);
 
-    $para = "gabrielh2007.scs@gmail.com";
-    $assunto = "Coleta de dados - Portfolio";
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "E-mail inválido.";
+    exit;
+}
 
-    $corpo = "Nome: " . $nome . "\n" . 
-             "E-mail: " . $email . "\n" . 
-             "Telefone: " . $contato . "\n" . 
-             "Mensagem: " . $mensagem;
+$para = "gabrielh2007.scs@gmail.com";
+$assunto = "Coleta de dados - Portfolio";
 
-    $cabeca = "From: gabrielh2007.scs@gmail.com" . "\n" . 
-              "Reply-to: " . $email . "\n" . "X=Mailer:PHP/" .phpversion();
+$corpo = "Nome: " . $nome . "\r\n" . 
+         "E-mail: " . $email . "\r\n" . 
+         "Telefone: " . $contato . "\r\n" . 
+         "Mensagem: " . $mensagem;
 
-    if (mail($para, $assunto, $corpo, $cabeca)) {
-        echo "E-mail enviado com sucesso!";
-    } else {
-        echo "Houve um erro ao enviar e-mail!";
-    }
+$cabeca = "From: gabrielh2007.scs@gmail.com\r\n" . 
+          "Reply-To: " . $email . "\r\n" .
+          "X-Mailer: PHP/" . phpversion();
+
+if (mail($para, $assunto, $corpo, $cabeca)) {
+    echo "E-mail enviado com sucesso!";
+} else {
+    echo "Houve um erro ao enviar o e-mail!";
+}
 
 ?>
